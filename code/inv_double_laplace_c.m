8c9c9f75ba21045d2690186ed6a2bf3a478c3ffc
Steven Wang <wx@bu.edu>
2016-04-22 21:22:12 -0400

    sort into functions

---
+++ code/inv_double_laplace_c.m
@@ -0,0 +1,80 @@
+function  v_c = inv_double_laplace_c(N,T,n,K,r,x,G)
+% 6 Compute f->v
+% Continuous case
+N = N;
+T = T;
+n = n;
+K = K;
+r = r;
+x = x;
+m1 = 10;
+m2 = 15;
+I = diag(ones(1,N));
+D = diag(x);
+v_c = zeros(N,1);
+A1 = 20;
+A2 = 20;
+t1 = n;    %time
+t2 = K/T;    %strike price
+
+for j = 0:m1    %approximation 2.1
+    mu = A1/(2*t1)-1i*pi/t1-1i*j*pi/t1;
+    sum3 = zeros(N,1);
+    for jj = 0:m2+j    %summation in approximation 2.1
+        sum2 = zeros(N,1);
+        for jjj = 0:m1    %approximation 1.1
+            sum1 = zeros(N,1);
+            for jjjj = 0:m2+jjj    %summation in approximation 1.1
+                theta = A2/(2*t2)-1i*pi/t2-1i*jjjj*pi/t2;
+                f_c = I/(theta*D + mu*I-G)*ones(N,1);
+                L_c = 1/(theta^2)*f_c - 1/((theta^2)*mu) + (x')/(theta*mu*(mu-r));
+                sum1 = sum1 + (-1)^jjjj*L_c;  
+            end       
+            sum2 = sum2 + factorial(m1)/(factorial(jjj)*factorial(m1-jjj))*2^(-m1)*sum1;
+        end
+        for jjj = 0:m1    %approximation 1.2
+            sum1 = zeros(N,1);
+            for jjjj = 0:m2+jjj    %summation in approximation 1.2
+                theta = A2/(2*t2)-1i*pi/t2-1i*(-jjjj)*pi/t2;
+                f_c = I/(theta*D + mu*I-G)*ones(N,1);
+                L_c = 1/(theta^2)*f_c - 1/((theta^2)*mu) + (x')/(theta*mu*(mu-r));
+                sum1 = sum1 + (-1)^jjjj*L_c;  
+            end       
+            sum2 = sum2 + factorial(m1)/(factorial(jjj)*factorial(m1-jjj))*2^(-m1)*sum1;
+        end
+        sum3 = sum3 + (-1)^jj*sum2;
+    end
+    v_c = v_c + factorial(m1)/(factorial(j)*factorial(m1-j))*2^(-m1)*sum3;
+end
+for j = 0:m1    %approximation 2.2
+    mu = A1/(2*t1)-1i*pi/t1-1i*(-j)*pi/t1;
+    sum3 = zeros(N,1);
+    for jj = 0:m2+j    %summation in approximation 2.2
+        sum2 = zeros(N,1);
+        for jjj = 0:m1    %approximation 1.1
+            sum1 = zeros(N,1);
+            for jjjj = 0:m2+jjj    %summation in approximation 1.1
+                theta = A2/(2*t2)-1i*pi/t2-1i*jjjj*pi/t2;
+                f_c = I/(theta*D + mu*I-G)*ones(N,1);
+                L_c = 1/(theta^2)*f_c - 1/((theta^2)*mu) + (x')/(theta*mu*(mu-r));
+                sum1 = sum1 + (-1)^jjjj*L_c;
+
+            end       
+            sum2 = sum2 + factorial(m1)/(factorial(jjj)*factorial(m1-jjj))*2^(-m1)*sum1;
+        end
+        for jjj = 0:m1    %approximation 1.2
+            sum1 = zeros(N,1);
+            for jjjj = 0:m2+jjj    %summation in approximation 1.2
+                theta = A2/(2*t2)-1i*pi/t2-1i*(-jjjj)*pi/t2;
+                f_c = I/(theta*D + mu*I-G)*ones(N,1);
+                L_c = 1/(theta^2)*f_c - 1/((theta^2)*mu) + (x')/(theta*mu*(mu-r));
+                sum1 = sum1 + (-1)^jjjj*L_c;  
+            end       
+            sum2 = sum2 + factorial(m1)/(factorial(jjj)*factorial(m1-jjj))*2^(-m1)*sum1;
+        end
+        sum3 = sum3 + (-1)^jj*sum2;        
+    end
+    v_c = v_c + factorial(m1)/(factorial(j)*factorial(m1-j))*2^(-m1)*sum3;
+end
+v_c = exp((A1+A2)/2)/(4*t1*t2)*v_c;
+end
---
+++ code/inv_double_laplace_d.m
@@ -0,0 +1,56 @@
+function  v_d = inv_double_laplace_d(N,T,n,K,r,x,G)
+% 6 Compute f->v 
+% Discrete case
+N = N;
+T = T;
+n = n;
+K = K;
+r = r;
+x = x;
+delta = T/N;
+P = exp(delta*G); %G->P
+m1 = 10;
+m2 = 15;
+I = diag(ones(1,N));
+D = diag(x);
+
+v_d = zeros(N,1);
+A = 20;
+n = T/delta; %discretization of time
+t = K/(n+1); %a form of strike price
+rou = nthroot(exp(-A), 2*n);
+
+for j = 0:m1   %approximation
+    theta = A/(2*t)-1i*pi/t-1i*j*pi/t;
+    sum3 = zeros(N,1);
+    for jj = 0:m2+j    %summation in approximation
+        sum2 = zeros(N,1);
+        for k = -n:(n-1)    %no approximation
+            z = rou*exp(1i*k*pi/n);
+            f_d = I/(exp(theta*D) - z*P)*ones(N,1);
+            L_d = 1/(theta^2)*f_d - 1/((theta^2)*(1-z)) + (x')/(theta*(1-z)*(1-z*exp(r*delta)));
+            sum2 = sum2 + (-1)^k*L_d;
+        end
+        sum3 = sum3 + (-1)^jj*sum2;
+    end
+    v_d = v_d + factorial(m1)/(factorial(j)*factorial(m1-j))*2^(-m1)*sum3;
+end
+for j = 0:m1   %approximation
+    theta = A/(2*t)-1i*pi/t-1i*(-j)*pi/t;
+    sum3 = zeros(N,1);
+    for jj = 0:m2+j    %summation in approximation
+        sum2 = zeros(N,1);
+        for k = -n:(n-1)    %no approximation
+            z = rou*exp(1i*k*pi/n);
+            f_d = I/(exp(theta*D) - z*P)*ones(N,1);
+            L_d = 1/(theta^2)*f_d - 1/((theta^2)*(1-z)) + (x')/(theta*(1-z)*(1-z*exp(r*delta)));
+            sum2 = sum2 + (-1)^k*L_d;
+        end
+        sum3 = sum3 + (-1)^jj*sum2;
+    end
+    v_d = v_d + factorial(m1)/(factorial(j)*factorial(m1-j))*2^(-m1)*sum3;
+end
+v_d = exp(A/2)/(4*n*rou^n*t)*v_d;
+
+
+end
